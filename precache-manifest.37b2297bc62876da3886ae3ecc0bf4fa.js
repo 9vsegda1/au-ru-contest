dd37027f298acdf2afb79ab7c9467fbf72d62a49
9vsegda1 <makki.nem@yandex.ru>
2020-09-12 13:06:22 +0700

    Updates

---
+++ precache-manifest.37b2297bc62876da3886ae3ecc0bf4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276b622da27f8fe0293cbee54d1d2ced",
+    "url": "/au-ru-contest/index.html"
+  },
+  {
+    "revision": "d5baaf388b2475d7203d",
+    "url": "/au-ru-contest/static/css/main.9f40d98b.chunk.css"
+  },
+  {
+    "revision": "7b57147c79f948656dbd",
+    "url": "/au-ru-contest/static/js/2.9de42c0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/au-ru-contest/static/js/2.9de42c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5baaf388b2475d7203d",
+    "url": "/au-ru-contest/static/js/main.7a2ffc5c.chunk.js"
+  },
+  {
+    "revision": "95c01d345e72be9f103f",
+    "url": "/au-ru-contest/static/js/runtime-main.66b37e94.js"
+  }
+]);
